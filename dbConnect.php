7e48464990c1f960dc99322b6dbfb970659b0f4b
SambaNoelN <sambanoelnformi>
2025-07-30 07:51:12 -0700

    catch errors in database connection

--- dbConnect.php
+++ dbConnect.php
@@ -5,3 +5,13 @@ $username='root';
 $password='';
 
 $conn=mysqli_connect($host,$username,$password,$dbname);
+
+try {
+    $conn =new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Connection successful
+}
+catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+    exit();
+}
